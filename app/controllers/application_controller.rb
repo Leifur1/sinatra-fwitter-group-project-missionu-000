6296c57d9185c1c93f7ecf532c7db24b204b2c24
Leifur Sigurdsson <leifurerik@gmail.com>
2019-01-16 23:03:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 require './config/environment'
 
+# sets root as the parent-directory of the current file
+set :root, File.join(File.dirname(__FILE__), '..')
+# sets the view directory correctly
+set :views, Proc.new { File.join(root, "views") }
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -7,4 +12,18 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'layout'
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
 end
